d6968c0c48d529b2c6f321c6e863f0b3ac455966
ragranato <robert.granato@gmail.com>
2024-12-18 21:32:42 -0500

    finished splitnodesdelimiter.py

--- src/splitnodesdelimiter.py
+++ src/splitnodesdelimiter.py
@@ -3,31 +3,42 @@ from htmlnode import *
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
+      
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
-        else:
-            text = node.text            
-            start = text.find(delimiter)
+            continue  # Skip further processing
+
+        text = node.text
+        i = 0  # Initialize `i`
+
+        while i < len(text):
+            start = text.find(delimiter, i)
             if start == -1:
-                new_nodes.append(node)
-                
-            else:
-                end = text.find(delimiter, start + len(delimiter))
-           
-                if end == -1:
-                    raise Exception("Invalid markdown syntax")
-                if len(text[start+len(delimiter):end]) == 0:
-                    raise Exception("Invalid markdown syntax")
-                if start > 0:
-                    new_nodes.append(TextNode(text[:start], TextType.TEXT))  
-                new_nodes.append(TextNode(text[start+len(delimiter):end], text_type))
-                remaining = text[end + len(delimiter):]
-                if remaining:
-                    new_nodes.append(TextNode(remaining, TextType.TEXT))
+                # If no start delimiter, add remaining text as a normal text node
+                new_nodes.append(TextNode(text[i:], TextType.TEXT))
+                break
+
+            # Add text before the first delimiter
+            if start > i:
+                new_nodes.append(TextNode(text[i:start], TextType.TEXT))
+
+            end = text.find(delimiter, start + len(delimiter))
+            if end == -1:
+                raise ValueError("Unmatched delimiter found in text.")
+
+            # Add the node text for the delimited text
+            new_nodes.append(TextNode(text[start + len(delimiter):end], text_type))
+
+            # Continue searching after the end delimiter
+            i = end + len(delimiter)
+            
     return new_nodes
-                    
-print(split_nodes_delimiter([TextNode("`code` hello `code` world", TextType.TEXT)], "`", TextType.CODE) )              
+
+                        
+
+
+
                 
 
     
