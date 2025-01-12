4335c23758893c374f6a8c6e56557f504a33e23e
ragranato <robert.granato@gmail.com>
2025-01-12 17:19:31 -0500

    working on text_to_textnodes.py

---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,31 @@
+import unittest
+
+from text_to_textnodes import *
+
+
+class TestTextToTextnodes(unittest.TestCase):
+    def test_eq(self):
+        image_list = text_to_textnodes("This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(image_list, expected)
+    def test_eq2_tag(self):
+        image_list = split_nodes_image([TextNode("This is a test of the coding network...", TextType.TEXT)])
+        expected = [
+            TextNode("This is a test of the coding network...", TextType.TEXT),            
+        ]
+        self.assertEqual(image_list, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -3,5 +3,19 @@ from splitnodesdelimiter import *
 
 def text_to_textnodes(text):
     new_nodes = []
-
+    delimiters = [('**', TextType.BOLD), ('*', TextType.ITALIC), ('`', TextType.CODE), ]
     
+    text_node = TextNode(text, TextType.TEXT)
+    
+    image_link_nodes = split_nodes_link(split_nodes_image([text_node]))
+
+    for d in delimiters:
+        temp_list = image_link_nodes
+        node_list = []
+        for node in image_link_nodes:
+            text_temp = split_nodes_delimiter([node], d[0], d[1])
+            node_list += text_temp        
+        new_nodes += node_list
+    return new_nodes
+
+print(text_to_textnodes('This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'))
